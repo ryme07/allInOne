1d12a7456565d30e988b8db8b1548c890ed840d3
ryme07 <remydelrieu@hotmail.fr>
2021-05-04 21:38:39 +0200

    feat: add helpers folder with error interceptor & fake backend

---
+++ src/app/helpers/error.interceptors.ts
@@ -0,0 +1,23 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, throwError } from "rxjs";
+import { catchError } from 'rxjs/operators'
+
+import { AlertService } from "@app/services/alert.service";
+
+
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+    constructor(private alertService: AlertService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(catchError(err => {
+            const error = err.error?.message || err.statusText;
+            this.alertService.error(error);
+            console.error(err);
+            return throwError(error);
+        }))
+    }
+}
---
+++ src/app/helpers/fake-backend.ts
@@ -0,0 +1,133 @@
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS, HttpResponse } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, materialize, dematerialize } from 'rxjs/operators';
+
+import { Role } from '@app/models';
+
+// array in local storage 
+const usersKey = 'users-key-example';
+const userJSON = localStorage.getItem(usersKey);
+let users: any[] = userJSON ? JSON.parse(userJSON) : [{
+    id: 1,
+    title: 'M',
+    firstName: 'Jean',
+    lastName: 'Dupont',
+    email: 'jean@dupont.com',
+    role: Role.User,
+    password: 'jean123'
+}];
+
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const { url, method, headers, body } = request;
+
+        return handleRoute();
+
+        function handleRoute() {
+            switch (true) {
+                case url.endsWith('/users') && method === 'GET':
+                    return getUsers();
+                case url.match(/\/users\/\d+$/) && method === 'GET':
+                    return getUserById();
+                case url.endsWith('/users') && method === 'POST':
+                    return createUser();
+                case url.match(/\/users\/\d+$/) && method === 'PUT':
+                    return updateUser();
+                case url.match(/\/users\/\d+$/) && method === 'DELETE':
+                    return deleteUser();
+                default:
+                    // pass through any requests not handled above
+                    return next.handle(request);
+            }
+        }
+
+        // route functions
+
+        function getUsers() {
+            return ok(users.map(user => basicDetails(user)))
+        }
+
+        function getUserById() {
+            const user = users.find(user => user.id === idFromUrl())
+            return ok(basicDetails(user));
+        }
+
+        function createUser() {
+            const user = body;
+
+            if (users.find(x => x.email === user.email)) {
+                return error(`User with the email ${user.email} already exist`)
+            }
+
+            user.id = newUserId();
+            delete user.confirmPassword;
+            users.push(user);
+            localStorage.setItem(usersKey, JSON.stringify(users));
+
+            return ok();
+        }
+
+        function updateUser() {
+
+            let params = body;
+            let user = users.find(user => user.id === idFromUrl());
+
+            if (params.email !== user.email && users.find(x => x.email === params.email)) {
+                return error(`User with the email ${params.email} already exists`);
+            }
+
+            // only update password if entered
+            if (!params.password) {
+                delete params.password;
+            }
+
+            // update and save user
+            Object.assign(user, params);
+            localStorage.setItem(usersKey, JSON.stringify(users));
+
+            return ok();
+        }
+
+        function deleteUser() {
+            users = users.filter(x => x.id !== idFromUrl());
+            localStorage.setItem(usersKey, JSON.stringify(users));
+            return ok();
+        }
+
+        // helper functions
+
+        function ok(body?: any) {
+            return of(new HttpResponse({ status: 200, body }))
+                .pipe(delay(500));
+        }
+
+        function error(message: any) {
+            return throwError({ error: { message } })
+                .pipe(materialize(), delay(500), dematerialize())
+        }
+
+        function basicDetails(user: any) {
+            const { id, title, firstName, lastName, email, role } = user;
+            return { id, title, firstName, lastName, email, role }
+        }
+
+        function idFromUrl() {
+            const urlParts = url.split('/');
+            return parseInt(urlParts[urlParts.length - 1]);
+        }
+
+        function newUserId() {
+            return users.length ? Math.max(...users.map(user => user.id)) + 1 : 1;
+        }
+    }
+}
+
+export const fakeBackendProvider = {
+    // use fake backend in place of Http service for backend-less development
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
